da4a2c4ee3964bf1b440eef3ecdd04342e3c61c2
nazarejose <nazarejosecontact@gmail.com>
2026-04-02 03:25:49 -0300

    fix: remove unused useEffect import

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,8 +1,6 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 
 export function useLocalStorage<T>(key: string, initialValue: T) {
-  // State to store our value
-  // Pass initial state function to useState so logic is only executed once
   const [storedValue, setStoredValue] = useState<T>(() => {
     try {
       const item = window.localStorage.getItem(key);
@@ -13,8 +11,6 @@ export function useLocalStorage<T>(key: string, initialValue: T) {
     }
   });
 
-  // Return a wrapped version of useState's setter function that
-  // persists the new value to localStorage.
   const setValue = (value: T | ((val: T) => T)) => {
     try {
       const valueToStore =
